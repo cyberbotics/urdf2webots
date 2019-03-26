0cf7a97cda2d8b274dcf490ab5a3263195f2d7fe
tahir1069 <tahir1069@gmail.com>
2019-03-26 08:25:10 +0100

    Update writeProto.py
    Checking if the Coordinate indexes are for triangle i.e. a tuple of 3 for Bounding Object

--- writeProto.py
+++ writeProto.py
@@ -189,8 +189,18 @@ def URDFBoundingObject(proto, link, level, boxCollision):
             proto.write((boundingLevel + 1) * indent + '}\n')
 
             proto.write((boundingLevel + 1) * indent + 'coordIndex [\n' + (boundingLevel + 2) * indent)
-            for value in boundingObject.geometry.trimesh.coordIndex:
-                proto.write('%d %d %d -1 ' % (value[0], value[1], value[2]))
+            if len(boundingObject.geometry.trimesh.coordIndex)%3 == 0:
+                step=3
+                stop=int(len(boundingObject.geometry.trimesh.coordIndex)/step)
+                limit = range(step,(stop+1)*step,step)
+	            updated_CoordIndex = []
+                for value in limit:
+		            updated_CoordIndex.append([boundingObject.geometry.trimesh.coordIndex[value-3], boundingObject.geometry.trimesh.coordIndex[value-2], boundingObject.geometry.trimesh.coordIndex[value-1]])
+                for value in updated_CoordIndex:
+                    proto.write('%d %d %d -1 ' % (value[0], value[1], value[2]))
+    	    else:
+                for value in boundingObject.geometry.trimesh.coordIndex:
+                    proto.write('%d %d %d -1 ' % (value[0], value[1], value[2]))
             proto.write('\n' + (boundingLevel + 1) * indent + ']\n')
             proto.write(boundingLevel * indent + '}\n')
 
