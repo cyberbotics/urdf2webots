4bc4f64a13cfd0c44c601b110d184b9d9424bd3b
DavidMansolino <David.Mansolino@cyberbotics.com>
2019-09-24 16:54:47 +0200

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='urdf2webots',
+    version='0.1',
+    scripts=[
+        'urdf2webots.py',
+        'gazebo_materials.py',
+        'math_utils.py',
+        'parserURDF.py',
+        'writeProto.py'
+    ],
+    author="Cyberbotics",
+    author_email="suuport@cyberbotics.com",
+    description="A converter between URDF and PROTO files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyberbotics/urdf2webots",
+    packages=setuptools.find_packages(),
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development'
+    ]
+ )
