86e251052257b1b8fc0b4b8b9b7571d1b83960e3
David Mansolino <David.Mansolino@cyberbotics.com>
2019-09-25 10:24:48 +0200

    Improve bin.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='urdf2webots',
-    version='0.2.5',
-    scripts=['urdf2webots.py'],
+    version='0.2.7',
+    scripts=['bin/urdf2webots'],
     author="Cyberbotics",
     author_email="support@cyberbotics.com",
     description="A converter between URDF and PROTO files.",
