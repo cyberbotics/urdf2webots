66c4eba6e0e77591e221cf564795fb3e53412789
David Mansolino <David.Mansolino@cyberbotics.com>
2020-05-06 15:44:24 +0200

    Update writeProto.py

--- urdf2webots/writeProto.py
+++ urdf2webots/writeProto.py
@@ -279,6 +279,7 @@ def URDFShape(proto, link, level, normal=False):
         else:
             if visualNode.material.name is not None:
                 visualNode.material.defName = computeDefName(visualNode.material.name)
+            if visualNode.material.defName is not None:
                 proto.write((shapeLevel + 1) * indent + 'appearance DEF %s PBRAppearance {\n' % visualNode.material.defName)
             else:
                 proto.write((shapeLevel + 1) * indent + 'appearance PBRAppearance {\n')
@@ -330,6 +331,7 @@ def URDFShape(proto, link, level, normal=False):
             else:
                 if visualNode.geometry.name is not None:
                     visualNode.geometry.defName = computeDefName(visualNode.geometry.name)
+                if visualNode.geometry.defName is not None:
                     proto.write((shapeLevel + 1) * indent + 'geometry DEF %s IndexedFaceSet {\n' % visualNode.geometry.defName)
                 else:
                     proto.write((shapeLevel + 1) * indent + 'geometry IndexedFaceSet {\n')
