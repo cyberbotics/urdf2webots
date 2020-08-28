17783180909d78efb77747194b0eb54c65151b2b
Simon Steinmann <52213164+Simon-Steinmann@users.noreply.github.com>
2020-08-28 16:25:04 +0200

    Update batch_conversion.py
    Co-authored-by: David Mansolino <mansolino.david@gmail.com>

--- batch_conversion.py
+++ batch_conversion.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
     optParser.add_option('--update-cfg', dest='cfgUpdate', action='store_true', default=False, help='No conversion. Only updates or creates .json config for every URDF file in "automatic_conversion/urdf".')
 
     options, args = optParser.parse_args()
-    bc = batchConversion(options.sourcePath, options.outPath)
+    bc = BatchConversion(options.sourcePath, options.outPath)
     Override = options.forceMesh
     if options.cfgUpdate:
         bc.check_all_configs()
