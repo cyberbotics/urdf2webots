518f4d7f2f98dc4e9b775091f4bb9a79d4d5e537
Simon Steinmann <52213164+Simon-Steinmann@users.noreply.github.com>
2020-09-17 15:12:15 +0200

    Update urdf2webots/importer.py
    Co-authored-by: David Mansolino <mansolino.david@gmail.com>

--- urdf2webots/importer.py
+++ urdf2webots/importer.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
     optParser.add_option('--rotation', dest='initRotation', default='0 1 0 0',
                          help='Set the rotation field of your PROTO file.')
     optParser.add_option('--init-pos', dest='initPos', default=None,
-                         help='Set the initial positions of your robot joints. Example: --init_pos="[1.2, 0.5, -1.5]" would '
+                         help='Set the initial positions of your robot joints. Example: --init-pos="[1.2, 0.5, -1.5]" would '
                          'set the first 3 joints of your robot to the specified values, and leave the rest with their '
                          'default value')
     options, args = optParser.parse_args()
