d058e359b490f2e45e1438784d1895246a9833d4
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2022-07-05 11:52:45 +0200

    Restore center of mass in the presence of inertia matrix (#179)
    * Restore CoM
    
    * Fix

--- urdf2webots/writeRobot.py
+++ urdf2webots/writeRobot.py
@@ -219,7 +219,9 @@ def writeLinkPhysics(robotFile, link, level):
     robotFile.write((level + 1) * indent + 'physics Physics {\n')
     robotFile.write((level + 2) * indent + 'density -1\n')
     robotFile.write((level + 2) * indent + 'mass %lf\n' % link.inertia.mass)
-    if link.inertia.position[0] != 0.0 and link.inertia.position[1] != 0.0 and link.inertia.position[2] != 0:
+    if (link.inertia.position[0] != 0.0 and link.inertia.position[1] != 0.0 and link.inertia.position[2] != 0 or
+        (link.inertia.ixx > 0.0 or link.inertia.iyy > 0.0 or link.inertia.izz > 0.0 or link.inertia.ixy > 0.0 or
+         link.inertia.ixz > 0.0 or link.inertia.ixy > 0.0)):
         robotFile.write((level + 2) * indent + 'centerOfMass [ %lf %lf %lf ]\n' % (link.inertia.position[0],
                                                                                    link.inertia.position[1],
                                                                                    link.inertia.position[2]))
