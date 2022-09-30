0078a45aaa918fa1243e468ef690f7517602b5c2
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-09-30 18:07:05 +0200

    Update setup.py (#188)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='urdf2webots',
-    version='2.0.0',
+    version='2.0.1',
     author='Cyberbotics',
     author_email='support@cyberbotics.com',
     description='A converter between URDF and PROTO files.',
